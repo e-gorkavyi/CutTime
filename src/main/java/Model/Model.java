08de9bf9276657d6348f580a2894755a681131c4
Evgeniy Gorkavyi <e.gorkavyi@gmail.com>
2023-08-08 21:52:24 +0600

    Shining in tunnel's end.

--- src/main/java/Model/Model.java
+++ src/main/java/Model/Model.java
@@ -1,50 +1,26 @@
 package Model;
 
 import org.kabeja.dxf.*;
+import org.kabeja.dxf.helpers.Point;
 import org.kabeja.parser.ParseException;
 import org.kabeja.parser.Parser;
 import org.kabeja.parser.ParserBuilder;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class Model{
+public class Model {
 
-    class Line extends DXFEntity implements DXFPrimitive {
-        double
-                x1,
-                y1,
-                x2,
-                y2,
-                length;
+    class Line extends DXFLine implements DXFPrimitive {
         PrimitiveType type = PrimitiveType.LINE;
 
-        public Line() {}
-
-        @Override
-        public Bounds getBounds() {
-            return null;
+        public Line() {
         }
 
         @Override
         public String getType() {
             return this.type.toString();
-        }
-
-        public Line(double x1, double y1, double x2, double y2) {
-            this.x1 = x1;
-            this.y1 = y1;
-            this.x2 = x2;
-            this.y2 = y2;
-            this.length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        }
-
-        public Line(double x1, double y1, double x2, double y2, double length) {
-            this.x1 = x1;
-            this.y1 = y1;
-            this.x2 = x2;
-            this.y2 = y2;
-            this.length = length;
         }
 
         @Override
@@ -54,27 +30,22 @@ public class Model{
 
         @Override
         public double getX1() {
-            return x1;
+            return this.getStartPoint().getX();
         }
 
         @Override
         public double getY1() {
-            return y1;
+            return this.getStartPoint().getY();
         }
 
         @Override
         public double getX2() {
-            return x2;
+            return this.getEndPoint().getX();
         }
 
         @Override
         public double getY2() {
-            return y2;
-        }
-
-        @Override
-        public double getLength() {
-            return this.length;
+            return this.getEndPoint().getY();
         }
     }
 
@@ -135,14 +106,14 @@ public class Model{
 
     public double lineAngle(Line ln) {
         double angle;
-        if (ln.x2 - ln.x1 != 0) {
-            angle = Math.toDegrees(Math.atan((ln.y2 - ln.y1) / (ln.x2 - ln.x1)));
+        if (ln.getX2() - ln.getX1() != 0) {
+            angle = Math.toDegrees(Math.atan((ln.getY2() - ln.getY1()) / (ln.getX2() - ln.getX1())));
         } else {
             angle = -90.0;
-            if (ln.y2 > ln.y1)
+            if (ln.getY2() > ln.getY1())
                 angle = 90.0;
         }
-        if (ln.x2 - ln.x1 < 0 && ln.y2 - ln.y1 < 0)
+        if (ln.getX2() - ln.getX1() < 0 && ln.getY2() - ln.getY1() < 0)
             angle = angle + 180;
         return angle;
     }
@@ -152,32 +123,46 @@ public class Model{
     }
 
     public void readDXF() throws ParseException {
-        ArrayList<DXFPrimitive> dxfPrimitives = new ArrayList<>();
-        Parser parser = ParserBuilder.createDefaultParser();
-        parser.parse("triangle_tuck-lock.dxf");
-        DXFDocument document = parser.getDocument();
-        DXFLayer layer = document.getDXFLayer("Design");
-        List<DXFPrimitive> primitives = new ArrayList<>();
-        List<DXFLine> lineList = new ArrayList<>();
-        if (layer.hasDXFEntities(DXFConstants.ENTITY_TYPE_LINE))
-            lineList.addAll(layer.getDXFEntities(DXFConstants.ENTITY_TYPE_LINE));
 
-        for (int i = 0; i < lineList.size(); i++) {
-            primitives.add(new Line(
-                    lineList.get(i).getStartPoint().getX(),
-                    lineList.get(i).getStartPoint().getY(),
-                    lineList.get(i).getEndPoint().getX(),
-                    lineList.get(i).getEndPoint().getY(),
-                    lineList.get(i).getLength()
-            ));
-        primitives.forEach((p) -> System.out.println(
-                        p.getPrimitiveType() +
-                        ", x1:" + p.getX1() +
-                        ", y1:" + p.getY1() +
-                        ", x2:" + p.getX2() +
-                        ", y2:" + p.getY2() +
-                        ", lenght:" + p.getLength()
-        ));
+        Parser parser = ParserBuilder.createDefaultParser();
+        parser.parse("111.dxf");
+        DXFDocument document = parser.getDocument();
+        List<DXFLine> dxfLines = new ArrayList<>();
+        List<DXFArc> dxfArcs = new ArrayList<>();
+        Iterator<DXFLayer> layerIterator = document.getDXFLayerIterator();
+        DXFLayer layer;
+        while (layerIterator.hasNext()) {
+            layer = layerIterator.next();
+            try {
+                dxfLines.addAll(layer.getDXFEntities(DXFConstants.ENTITY_TYPE_LINE));
+            } catch (Exception ignored) {}
+            try {
+                dxfArcs.addAll(layer.getDXFEntities(DXFConstants.ENTITY_TYPE_ARC));
+            } catch (Exception ignored) {}
+        }
+
+        for (DXFLine line : dxfLines) {
+            System.out.println(line.getLength() +
+                    " " +
+                    line.getStartPoint().toString() +
+                    " " +
+                    line.getEndPoint().toString());
+        }
+
+        for (DXFArc arc : dxfArcs) {
+            System.out.println(arc.getStartPoint().getX() +
+                    "/" +
+                    arc.getStartPoint().getY() +
+                    " " +
+                    arc.getEndPoint().getX() +
+                    "/" +
+                    arc.getEndPoint().getY() +
+                    " " +
+                    arc.getCenterPoint().getX() +
+                    " " +
+                    arc.getCenterPoint().getY() +
+                    " " +
+                    arc.getRadius());
         }
     }
 }
